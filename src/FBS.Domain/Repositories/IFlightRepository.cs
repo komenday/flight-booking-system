fbb03cf3f421a9cd4ddfc5fa8eed4503494c6fbe
Yurii Komenda <yurii.komenda@dataart.com>
2026-03-04 15:53:22 +0200

    #27  repository-optimization

--- src/FBS.Domain/Repositories/IFlightRepository.cs
+++ src/FBS.Domain/Repositories/IFlightRepository.cs
@@ -1,18 +1,17 @@
 ﻿using FBS.Domain.Common.Interfaces;
 using FBS.Domain.Flight;
-using FBS.Domain.SharedKernel;
 
 namespace FBS.Domain.Repositories;
 
 public interface IFlightRepository
 {
-    Task<Flight.Flight?> GetByIdAsync(FlightId id, CancellationToken cancellationToken);
+    ValueTask<Flight.Flight?> GetByIdAsync(FlightId id, CancellationToken cancellationToken);
 
     Task<IReadOnlyList<Flight.Flight>> GetAsync(ISpecification<Flight.Flight> spec, CancellationToken cancellationToken);
 
     Task<Flight.Flight?> GetFirstOrDefaultAsync(ISpecification<Flight.Flight> spec, CancellationToken cancellationToken);
 
-    Task AddAsync(Flight.Flight flight, CancellationToken cancellationToken);
+    ValueTask AddAsync(Flight.Flight flight, CancellationToken cancellationToken);
 
-    Task UpdateAsync(Flight.Flight flight, CancellationToken cancellationToken);
+    ValueTask UpdateAsync(Flight.Flight flight, CancellationToken cancellationToken);
 }
